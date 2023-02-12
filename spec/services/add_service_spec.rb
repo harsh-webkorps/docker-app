7c19297b56c5cb2ce066fd950f7a0c18aa1d4c30
Harsh <harshwardhan@webkorps.com>
2023-02-12 12:00:50 +0530

    fixed spec

--- spec/services/add_service_spec.rb
+++ spec/services/add_service_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe AddService do
   context 'add' do
     it 'adds two numbers' do
       result = AddService.new.add(1,2)
-      expect(result).to eq(4)
+      expect(result).to eq(3)
     end
   end
 end
